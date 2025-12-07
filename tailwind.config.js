3364d6f6154b49a5671e2b5610144c2c472da794
seohyun-lee <seohyun-lee@users.noreply.github.com>
2025-12-07 04:41:12 +0000

    deploy: a085664f772522fab73757af659c6d38d7475cee

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./main.jsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./game/**/*.{js,ts,jsx,tsx,html}",
+    "./web/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
